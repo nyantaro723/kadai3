4d6b4064efb7d50038c3fe9ea6fde5bb4e617f68
Ryusei Matsuki <ryusei.1210@icloud.com>
2024-01-13 22:40:38 +0900

    Update test

--- test.bash
+++ test.bash
@@ -10,25 +10,25 @@ ng() {
 res=0
 
 ### PRIME FACTORIZATION TEST ###
-out=$(echo "12" | python prime_factorization.py)
+out=$(echo "12" | ./prime_factorization
 expected="12 の素因数分解結果: [2, 2, 3]"
 echo "out: ${out}"
 echo "expected: ${expected}"
 [ "${out}" == "${expected}" ] || ng $LINENO
 
-out=$(echo "60" | python prime_factorization.py)
+out=$(echo "60" | ./prime_factorization)
 expected="60 の素因数分解結果: [2, 2, 3, 5]"
 echo "out: ${out}"
 echo "expected: ${expected}"
 [ "${out}" == "${expected}" ] || ng $LINENO
 
-out=$(echo "97" | python prime_factorization.py)
+out=$(echo "97" | ./prime_factorization)
 expected="97 の素因数分解結果: [97]"
 echo "out: ${out}"
 echo "expected: ${expected}"
 [ "${out}" == "${expected}" ] || ng $LINENO
 
-out=$(echo "1" | python prime_factorization.py)
+out=$(echo "1" | ./prime_factorization)
 expected="1 の素因数分解結果: []"
 echo "out: ${out}"
 echo "expected: ${expected}"
